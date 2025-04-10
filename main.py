b04699ecfd2f1a7ff3636f7aa041838e0ef688ed
Drosos <drosinakis.drosos@gmail.com>
2025-04-11 00:37:34 +0300

    make it to work args you pass the file

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import sys
+
 from stats import count_letters, count_text_words, stats_report
 
 
 def main():
-    frank = "books/frankenstein.txt"
-    book = get_book_text(frank)
+    args = sys.argv
+    if len(args) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book = get_book_text(args[1])
     words = count_text_words(book)
     letters = count_letters(book)
     stats_report(words, letters)
