2781728c5b3e57d586810349ce24f78ac652879e
Drosos <drosinakis.drosos@gmail.com>
2025-04-11 00:18:53 +0300

    add the count_letter function to find letter occurrences

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from stats import count_text_words
+from stats import count_letters, count_text_words
 
 
 def main():
     frank = "books/frankenstein.txt"
     book = get_book_text(frank)
     print(f"{count_text_words(book)} words found in the document")
+    letters = count_letters(book)
+    for letter, occurrences in letters.items():
+        print(f"'{letter}': {occurrences}")
 
 
 def get_book_text(path):
--- stats.py
+++ stats.py
@@ -1,2 +1,13 @@
 def count_text_words(book):
     return len(book.split())
+
+
+def count_letters(book):
+    letter_occurrences = {}
+    for letter in book:
+        l_letter = letter.lower()
+        if l_letter in letter_occurrences:
+            letter_occurrences[l_letter] += 1
+        else:
+            letter_occurrences[l_letter] = 1
+    return letter_occurrences
